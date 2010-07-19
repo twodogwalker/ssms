33471c68a6132833b84eb26650cb6afc5b12042e
TwoDogWalker <TwoDogWalker@c554adb8-e973-17ed-6c63-943ca85b2b96>
2010-07-19 20:54:54 +0000

    work in progress... removed the new sms button, started an options menu

--- trunk/ssms/src/nl/frankprins/ssms/R.java
+++ trunk/ssms/src/nl/frankprins/ssms/R.java
@@ -14,15 +14,17 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int bottomtext=0x7f050001;
-        public static final int boxname=0x7f050007;
-        public static final int entry=0x7f050003;
-        public static final int label=0x7f050002;
-        public static final int msgBody=0x7f050005;
-        public static final int msgCharCount=0x7f050006;
-        public static final int num_total=0x7f050008;
-        public static final int select=0x7f050004;
-        public static final int toptext=0x7f050000;
+        public static final int bottomtext=0x7f060001;
+        public static final int boxname=0x7f060007;
+        public static final int entry=0x7f060003;
+        public static final int label=0x7f060002;
+        public static final int msgBody=0x7f060005;
+        public static final int msgCharCount=0x7f060006;
+        public static final int new_sms=0x7f060009;
+        public static final int num_total=0x7f060008;
+        public static final int quit=0x7f06000a;
+        public static final int select=0x7f060004;
+        public static final int toptext=0x7f060000;
     }
     public static final class layout {
         public static final int box=0x7f020000;
@@ -31,15 +33,19 @@ public final class R {
         public static final int smsbox_row=0x7f020003;
         public static final int smsboxes=0x7f020004;
     }
+    public static final class menu {
+        public static final int main_menu=0x7f050000;
+    }
     public static final class string {
         public static final int app_name=0x7f030000;
-        public static final int main_no_items=0x7f030002;
-        public static final int messageComposeHint=0x7f030008;
-        public static final int messageListNamePrefixFrom=0x7f030003;
-        public static final int messageListNamePrefixTo=0x7f030004;
-        public static final int messageNewCounterLabel=0x7f030007;
-        public static final int messageNewFindButtonText=0x7f030005;
-        public static final int messageNewMessageLabel=0x7f030006;
-        public static final int new_sms_button=0x7f030001;
+        public static final int main_no_items=0x7f030003;
+        public static final int menu_item_new_sms=0x7f030001;
+        public static final int menu_item_quit=0x7f030002;
+        public static final int messageComposeHint=0x7f030009;
+        public static final int messageListNamePrefixFrom=0x7f030004;
+        public static final int messageListNamePrefixTo=0x7f030005;
+        public static final int messageNewCounterLabel=0x7f030008;
+        public static final int messageNewFindButtonText=0x7f030006;
+        public static final int messageNewMessageLabel=0x7f030007;
     }
 }
