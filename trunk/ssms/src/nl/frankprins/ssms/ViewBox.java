79eb93b58a839f372964d14cf3d70d7e5956d05c
TwoDogWalker <TwoDogWalker@c554adb8-e973-17ed-6c63-943ca85b2b96>
2010-11-09 15:33:04 +0000

    work in progress... made the message listings "lazy loading", e.g. have only a fixed number of messages load, until you scroll to the end of the list, and another set will be loaded and aded to the list. A nice performance improvement...

--- trunk/ssms/src/nl/frankprins/ssms/ViewBox.java
+++ trunk/ssms/src/nl/frankprins/ssms/ViewBox.java
@@ -13,7 +13,10 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.TextView;
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -26,15 +29,21 @@ import nl.frankprins.ssms.ClickableListAdapter.ViewHolder;
  *
  * @author Frank
  */
-public class ViewBox extends ListActivity {
+public class ViewBox extends ListActivity implements OnScrollListener {
 
   private static final String TAG = "SSMS-Debug";
   private ArrayList<SmsMessage> messageList = null;
   MyClickableListAdapter myAdapter;
   private ProgressDialog m_ProgressDialog = null;
   private Runnable viewMessages;
+// TODO put default listsize into preferences
+  private int listSize = 15, shown = 0;
+  //TODO put date format into preferences
   private final String dateFormat = "MMM dd, yyyy HH:mm";
   Format formatter = new SimpleDateFormat(dateFormat);
+  String boxName = "";
+  Uri boxUri = null;
+  Cursor cursor;
 
   @Override
   public void onCreate(Bundle bundle) {
@@ -48,16 +57,21 @@ public class ViewBox extends ListActivity {
         boxToView = box;
       }
     }
-    final String boxName = box;
+    boxName = box;
     setTitle(boxToView);
-    final Uri boxUri = Uri.parse("content://sms/" + boxToView);
+    boxUri = Uri.parse("content://sms/" + boxToView);
     messageList = new ArrayList<SmsMessage>();
+    cursor = getContentResolver().query(boxUri,
+            new String[]{"_id", "thread_id", "address", "person", "date", "body"}, null, null, null);
+    startManagingCursor(cursor);
     // load messages
     viewMessages = new Runnable() {
 
       @Override
       public void run() {
-        getMessages(boxName, boxUri);
+        List messages = getMessages(boxName, 0);
+        messageList.addAll(messages);
+        shown = messageList.size();
       }
     };
     Thread thread = new Thread(null, viewMessages, "MagentoBackground");
@@ -66,6 +80,7 @@ public class ViewBox extends ListActivity {
     myAdapter = new MyClickableListAdapter(this, R.layout.listrow, messageList, boxToView);
     this.getListView().setFastScrollEnabled(true);
     setListAdapter(myAdapter);
+    getListView().setOnScrollListener(this);
   }
   private Runnable returnRes = new Runnable() {
 
@@ -79,15 +94,23 @@ public class ViewBox extends ListActivity {
     }
   };
 
-  private void getMessages(String boxName, Uri boxUri) {
-    int count = 0;
-    messageList = new ArrayList<SmsMessage>();
-    Cursor cursor = getContentResolver().query(boxUri,
-            new String[]{"_id", "thread_id", "address", "person", "date", "body"}, null, null, null);
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+      cursor.close();
+    }
+    return super.onKeyDown(keyCode, event);
+  }
+
+  private ArrayList<SmsMessage> getMessages(String boxName, int start) {
+    ArrayList<SmsMessage> requestList = new ArrayList<SmsMessage>();
+    int count = 0, size = 0;
     if (cursor != null) {
       count = cursor.getCount();
       if (count > 0) {
-        while (cursor.moveToNext()) {
+        cursor.moveToPosition(start - 1);
+        while (cursor.moveToNext() && size <= listSize) {
+          size++;
           SmsMessage m = new SmsMessage();
           m.setMessageId(cursor.getLong(0));
           m.setThreadId(cursor.getLong(1));
@@ -98,12 +121,39 @@ public class ViewBox extends ListActivity {
           m.setContactId(cursor.getLong(3));
           m.setTimestamp(cursor.getLong(4));
           m.setBody(cursor.getString(5));
-          messageList.add(m);
+          requestList.add(m);
         }
       }
     }
-    cursor.close();
+    //cursor.close();
     runOnUiThread(returnRes);
+    return requestList;
+  }
+
+  public void onScrollStateChanged(AbsListView view, int scrollState) {
+    if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+      int first = view.getFirstVisiblePosition();
+      int count = view.getChildCount();
+      if (first + count >= shown) {
+        // load messages
+        viewMessages = new Runnable() {
+
+          @Override
+          public void run() {
+            List messages = getMessages(boxName, shown);
+            messageList.addAll(messages);
+            shown = messageList.size();
+          }
+        };
+        Thread thread = new Thread(null, viewMessages, "MagentoBackground");
+        thread.start();
+        m_ProgressDialog = ProgressDialog.show(ViewBox.this, "", "loading...", true);
+      }
+    }
+  }
+
+  public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+          int totalItemCount) {
   }
 
   static class MyViewHolder extends ViewHolder {
@@ -138,13 +188,12 @@ public class ViewBox extends ListActivity {
       }
       mvh.firstRow.setText(namePreFix + ": " + message.getAddressName()
               + ", on:" + formatDate(message.getTimestamp()));
-      Log.d(TAG, String.valueOf(message.getBody().length()));
-      if(message.getBody().length() > 36) {
+      if (message.getBody().length() > 36) {
         mvh.secondRow.setText(message.getBody().substring(0, 35));
       } else {
         mvh.secondRow.setText(message.getBody());
       }
-      
+
     }
 
     @Override
