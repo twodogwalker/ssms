599b49e1ad556e2227c43f981e7185ec2f8291e8
TwoDogWalker <TwoDogWalker@c554adb8-e973-17ed-6c63-943ca85b2b96>
2010-11-19 20:26:36 +0000

    - enhanced the lists by having them loaded just a small amount of messages - possibility to swype on a list entry to reply, nice but needs some work though - further work on send activity, an almost working version now

--- ssms/src/nl/frankprins/ssms/R.java
+++ ssms/src/nl/frankprins/ssms/R.java
@@ -14,24 +14,35 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int bottomtext=0x7f060001;
-        public static final int boxname=0x7f060007;
-        public static final int entry=0x7f060003;
-        public static final int label=0x7f060002;
-        public static final int msgBody=0x7f060005;
-        public static final int msgCharCount=0x7f060006;
-        public static final int new_sms=0x7f060009;
-        public static final int num_total=0x7f060008;
-        public static final int quit=0x7f06000a;
-        public static final int select=0x7f060004;
-        public static final int toptext=0x7f060000;
+        public static final int LinearRow1=0x7f060002;
+        public static final int LinearRow2=0x7f060006;
+        public static final int bottomtext=0x7f06000a;
+        public static final int boxname=0x7f060011;
+        public static final int entry=0x7f06000c;
+        public static final int inBtn=0x7f060003;
+        public static final int label=0x7f06000b;
+        public static final int msgBody=0x7f06000e;
+        public static final int msgCharCount=0x7f06000f;
+        public static final int newBtn=0x7f060007;
+        public static final int new_sms=0x7f060013;
+        public static final int num_total=0x7f060012;
+        public static final int outBtn=0x7f060004;
+        public static final int quit=0x7f060014;
+        public static final int root=0x7f060000;
+        public static final int row1=0x7f060001;
+        public static final int row2=0x7f060005;
+        public static final int select=0x7f06000d;
+        public static final int sendButton=0x7f060010;
+        public static final int settingsBtn=0x7f060008;
+        public static final int toptext=0x7f060009;
     }
     public static final class layout {
         public static final int box=0x7f020000;
-        public static final int listrow=0x7f020001;
-        public static final int new_sms_form=0x7f020002;
-        public static final int smsbox_row=0x7f020003;
-        public static final int smsboxes=0x7f020004;
+        public static final int dashboard=0x7f020001;
+        public static final int listrow=0x7f020002;
+        public static final int new_sms_form=0x7f020003;
+        public static final int smsbox_row=0x7f020004;
+        public static final int smsboxes=0x7f020005;
     }
     public static final class menu {
         public static final int main_menu=0x7f050000;
@@ -41,11 +52,12 @@ public final class R {
         public static final int main_no_items=0x7f030003;
         public static final int menu_item_new_sms=0x7f030001;
         public static final int menu_item_quit=0x7f030002;
-        public static final int messageComposeHint=0x7f030009;
+        public static final int messageComposeHint=0x7f03000a;
         public static final int messageListNamePrefixFrom=0x7f030004;
         public static final int messageListNamePrefixTo=0x7f030005;
         public static final int messageNewCounterLabel=0x7f030008;
         public static final int messageNewFindButtonText=0x7f030006;
         public static final int messageNewMessageLabel=0x7f030007;
+        public static final int messageNewSendButtonText=0x7f030009;
     }
 }
